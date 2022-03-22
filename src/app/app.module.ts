b9299fbdb87610abd9ebb3d6efcc4b22006f8ab3
Geoffrey Morizot <gffr.mrzt@gmail.com>
2022-03-22 12:39:01 +0100

    feat: remove default routing files and implement manual routes for registration, user and default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { HighlightDirective } from './highlight.directive';
 import { DisplayGuestsDirective } from './display-guests.directive';
 import { HighlightFirstDirective } from './highlight-first.directive';
 import { DisplayFilmDirective } from './display-film.directive';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, UserProfileComponent, HighlightDirective, DisplayGuestsDirective, HighlightFirstDirective, DisplayFilmDirective],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    UserProfileComponent,
+    HighlightDirective,
+    DisplayGuestsDirective,
+    HighlightFirstDirective,
+    DisplayFilmDirective,
+    MenuComponent,
+    SignupComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
 })
